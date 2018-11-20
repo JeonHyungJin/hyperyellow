ebaaa3832a38f3686476c733e1c8713f32c02d50
JeonHyungJin <qpdjgjsl@gmail.com>
2018-11-21 00:00:23 +0900

    hyperblue rebuild

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
-    borderColor: 'yellow',
-    cursorColor: 'yellow',
+    borderColor: 'blue',
+    cursorColor: 'blue',
     css: `
       ${config.css || ''}
       .tabs_nav .tabs_list .tab_text {
-        color: yellow;
+        color: blue;
       }
       .tabs_nav .tabs_title {
-        color: yellow;
+        color: blue;
       }
     `
   });
